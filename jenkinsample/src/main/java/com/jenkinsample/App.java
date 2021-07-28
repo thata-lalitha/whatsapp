30249dfcc2a058d22eb819695b4cc0ce3fe79e9d
thata-lalitha <lalithathatha@gmail.com>
2021-07-28 12:07:20 +0530

    initial commit

---
+++ jenkinsample/src/main/java/com/jenkinsample/App.java
@@ -0,0 +1,13 @@
+package com.jenkinsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jenkinsample/src/main/java/com/jenkinsample/Jenkins.java
@@ -0,0 +1,10 @@
+package com.jenkinsample;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
